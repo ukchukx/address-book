bfc03cefe3e3b763d2cc5275e63b16051bc4d856
Uk <ukchukundah@gmail.com>
2019-06-23 13:38:15 +0100

    Update stale redirect

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -5,22 +5,18 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
-    public function handle($request, Closure $next, $guard = null)
-    {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
+class RedirectIfAuthenticated {
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @param  string|null  $guard
+   * @return mixed
+   */
+  public function handle($request, Closure $next, $guard = null) {
+    if (Auth::guard($guard)->check()) return redirect('/contacts');
 
-        return $next($request);
-    }
+    return $next($request);
+  }
 }
